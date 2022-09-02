19e1320abe200c96df00b14b007771b53d2b6efc
DI <cdi122@nate.com>
2022-09-02 22:57:45 +0900

    피자가게 목록 어댑터 뼈대작업

--- app/src/main/java/com/example/pizzaorderapp_dain/adapters/PizzaSotreAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp_dain/adapters/PizzaSotreAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.pizzaorderapp_dain.R
 import com.example.pizzaorderapp_dain.datas.Store
 
 class PizzaStoreAdapter(
@@ -15,7 +16,13 @@ class PizzaStoreAdapter(
 
         val inflater = LayoutInflater.from(mContext)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return super.getView(position, convertView, parent)
+       var tempRow = convertView
+        if (tempRow == null){
+            tempRow = inflater.inflate(R.layout.pizza_store_list_item, null)
+        }
+        val row = tempRow!!
+
+        return row
     }
 
 }
