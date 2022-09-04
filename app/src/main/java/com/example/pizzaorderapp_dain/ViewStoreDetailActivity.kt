ae60637590d0ad8dafed48430234465ea56e9a7b
DI <cdi122@nate.com>
2022-09-04 16:42:20 +0900

    상세 화면 실제 데이터 반영

--- app/src/main/java/com/example/pizzaorderapp_dain/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_dain/ViewStoreDetailActivity.kt
@@ -1,19 +1,32 @@
 package com.example.pizzaorderapp_dain
 
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.pizzaorderapp_dain.datas.Store
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActivity() {
+
+    lateinit var mStoreData : Store
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
         setupEvents()
         setValues()
+
     }
     override fun setupEvents() {
+
 
     }
 
     override fun setValues() {
+
+        mStoreData = intent.getSerializableExtra("storeData") as Store
+        Glide.with(mContext).load(mStoreData.logoURL).into(storeLogoImg)
+        storePhoneNumText.text = mStoreData.phoneNum
+        storeNameTxt.text = mStoreData.name
+
 
     }
 
